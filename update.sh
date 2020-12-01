ca5112fe55afab2a00738c7ceae9371b52330597
Eugene Kim <komastar.dev@gmail.com>
2020-12-01 23:01:38 +0900

    update.sh 서비스 이름 수정

--- update.sh
+++ update.sh
@@ -4,10 +4,10 @@ git pull
 echo "NGINX STOP"
 sudo service nginx stop
 echo "ORAK ASSET WEB STOP"
-sudo service kestrel-orakassetweb stop
+sudo service kestrel-webasset stop
 echo "PROJECT PUBLISH"
 dotnet publish --configuration release
 echo "ORAK ASSET WEB START"
-sudo service kestrel-orakassetweb start
+sudo service kestrel-webasset start
 echo "NGINX START"
 sudo service nginx start
