a2f3195dc90bce75969cb970f3b46dfbf95f7ac9
Eugene Kim <komastar.dev@gmail.com>
2020-12-23 14:50:02 +0900

    update.sh nginx 켜고 끄기 제거

--- update.sh
+++ update.sh
@@ -1,13 +1,9 @@
 echo "UPDATE START"
 echo "GIT PULL"
 git pull
-echo "NGINX STOP"
-sudo service nginx stop
 echo "ASSET WEB STOP"
 sudo service kestrel-wam stop
 echo "PROJECT PUBLISH"
 dotnet publish --configuration release
 echo "ASSET WEB START"
 sudo service kestrel-wam start
-echo "NGINX START"
-sudo service nginx start
